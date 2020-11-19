7718281519b8eac630241d662aef3909e5698789
TOBIAS BINETT <tobias_binett@hotmail.se>
2020-11-19 17:04:27 +0100

    Added Logic

--- UppgiftNummer1/Program.cs
+++ UppgiftNummer1/Program.cs
@@ -9,7 +9,7 @@ namespace UppgiftNummer1
             //Change title to "Inlämningsuppgift 1"
             Console.Title = "Inlämningsuppgift1";
 
-            double result;
+            double res;
 
             Console.Write(">Enter first operator: ");//Ask for operators!            
             char ops1 = Console.ReadKey().KeyChar;//input from the user
@@ -17,17 +17,108 @@ namespace UppgiftNummer1
             Console.Write(">Enter first operator: ");//Ask for operators!
             char ops2 = Console.ReadKey().KeyChar;//input from the user
             Console.Write("\n");
-
+            //Ask for terms
             Console.Write(">Enter first term: ");
             double firstTerm = double.Parse(Console.ReadLine());
             Console.Write(">Enter second term: ");
             double secondTerm = double.Parse(Console.ReadLine());
-            Console.Write("Enter third term: ");
+            Console.Write(">Enter third term: ");
             double thirdTerm = double.Parse(Console.ReadLine());
 
-            
 
+            if (ops1 == '+')
+            {
+                if (ops2 == '+')
+                {
+                    res = firstTerm + secondTerm + thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '-')
+                {
+                    res = firstTerm + secondTerm - thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '*')
+                {
+                    res = firstTerm + secondTerm * thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '/')
+                {
+                    res = firstTerm + secondTerm / thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
             }
+            if (ops1 == '-')
+            {
+                if (ops2 == '+')
+                {
+                    res = firstTerm - secondTerm + thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '-')
+                {
+                    res = firstTerm - secondTerm - thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '*')
+                {
+                    res = firstTerm - secondTerm * thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '/')
+                {
+                    res = firstTerm - secondTerm / thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+            }
+            if (ops1 == '*')
+            {
+                if (ops2 == '+')
+                {
+                    res = firstTerm * secondTerm + thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '-')
+                {
+                    res = firstTerm * secondTerm - thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '*')
+                {
+                    res = firstTerm * secondTerm * thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '/')
+                {
+                    res = firstTerm * secondTerm / thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+            }
+            if (ops1 == '/')
+            {
+                if (ops2 == '/')
+                {
+                    res = firstTerm / secondTerm + thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '-')
+                {
+                    res = firstTerm / secondTerm - thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '*')
+                {
+                    res = firstTerm / secondTerm * thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+                else if (ops2 == '/')
+                {
+                    res = firstTerm / secondTerm / thirdTerm;
+                    Console.WriteLine($">{firstTerm} {ops1} {secondTerm} {ops2} {thirdTerm} = {res}");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
