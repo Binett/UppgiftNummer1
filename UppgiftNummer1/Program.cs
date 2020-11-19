46f4b6acd870f9a5a44bd53505e5ddbf57c0258d
TOBIAS BINETT <tobias_binett@hotmail.se>
2020-11-19 16:43:48 +0100

    Added inputs

--- UppgiftNummer1/Program.cs
+++ UppgiftNummer1/Program.cs
@@ -6,7 +6,28 @@ namespace UppgiftNummer1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Change title to "Inlämningsuppgift 1"
+            Console.Title = "Inlämningsuppgift1";
+
+            double result;
+
+            Console.Write(">Enter first operator: ");//Ask for operators!            
+            char ops1 = Console.ReadKey().KeyChar;//input from the user
+            Console.Write("\n");
+            Console.Write(">Enter first operator: ");//Ask for operators!
+            char ops2 = Console.ReadKey().KeyChar;//input from the user
+            Console.Write("\n");
+
+            Console.Write(">Enter first term: ");
+            double firstTerm = double.Parse(Console.ReadLine());
+            Console.Write(">Enter second term: ");
+            double secondTerm = double.Parse(Console.ReadLine());
+            Console.Write("Enter third term: ");
+            double thirdTerm = double.Parse(Console.ReadLine());
+
+            
+
+            }
         }
     }
 }
